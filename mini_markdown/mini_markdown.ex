0da373808443618763cfd13d5efa2585205989fd
Mark Gregan <mark.g@hostdime.com>
2020-01-03 14:21:28 -0800

    Completing mini markdown

---
+++ mini_markdown/mini_markdown.ex
@@ -0,0 +1,21 @@
+defmodule MiniMarkdown do
+  def to_html(text) do
+    # do lots of transforms in pipeline
+    text
+    |> p
+    |> bold
+    |> italics
+  end
+
+  def italics(text) do
+    Regex.replace(~r/\*(.*)\*/, text, "<i>\\1</i>")
+  end
+
+  def bold(text) do
+    Regex.replace(~r/\*\*(.*)\*\*/, text, "<b>\\1</b>")
+  end
+
+  def p(text) do
+    Regex.replace(~r/(\r|\n|\r\n|^)+([^\r\n]+)((\r\n|\r|\n)$)?/, text, "<p>\\2</p>")
+  end
+end
